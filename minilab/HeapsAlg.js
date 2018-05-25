6153cbc9f627561a81fa2a1fab221880740729f0
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-25 19:31:27 -0400

    Finished exhaustive search

--- minilab/HeapsAlg.js
+++ minilab/HeapsAlg.js
@@ -1,39 +1,23 @@
-
-
-const nextElement = (n, set) => {
-  if(n === 1) {
-    console.log(set);
-    // TODO: Call an outside function to compute distance on this set
+const getPermutations = (set, App, size = set.length) => {
+  if (size === 1) {
+    App.exhaustiveSearch(set);
+    return;
   }
-  else {
-    for(let i = 0; i < n-1 ; i++) {
-      nextElement(n - 1, set);
-      if(n%2===0) {
-        let x = set[i];
-        set[i] = set[n-1];
-        set[n-1] = x;
-      }
-      else {
-        let x = set[0];
-        set[0] = set[n-1];
-        set[n-1] = x;
-      }
+  for (let i = 0; i < size - 1; i++) {
+    getPermutations(set, App, size - 1);
+
+    if (size % 2 == 1) {
+      const temp = set[0];
+      set[0] = set[size - 1];
+      set[size - 1] = temp;
+    } else {
+      const temp = set[i];
+      set[i] = set[size - 1];
+      set[size - 1] = temp;
     }
-    nextElement(n - 1, set);
   }
-}
 
-const countSet = (set) => {
-  let n = set.length;
-  
-  let array = [];
-  set.forEach((each) => {
-    array.push(each);
-  });
-  nextElement(array.length, array);
-}
+  getPermutations(set, App, size - 1);
+};
 
-countSet([1,2,3]);
-countSet(['a','b','c','d']);
-countSet(['corn','turkey','winter','algebra','window', 'kevin']);
-
+module.exports = getPermutations;
